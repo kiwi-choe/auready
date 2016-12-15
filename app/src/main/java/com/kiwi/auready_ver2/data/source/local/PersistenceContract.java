4881e9bf00e36d7b3ebe3b2fd2fbdef78f909e72
kiwi-choe <magmag0221@gmail.com>
2016-12-15 13:02:33 +0900

    Make package-local

--- app/src/main/java/com/kiwi/auready_ver2/data/source/local/PersistenceContract.java
+++ app/src/main/java/com/kiwi/auready_ver2/data/source/local/PersistenceContract.java
@@ -3,39 +3,39 @@ package com.kiwi.auready_ver2.data.source.local;
 /**
  * The contract used for the db to save tables locally.
  */
-public final class PersistenceContract {
+final class PersistenceContract {
 
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
     public PersistenceContract() {}
 
     /* TAG */
-    public static abstract class DBExceptionTag {
-        public static final String TAG_SQLITE = "SQLiteException: ";
+    static abstract class DBExceptionTag {
+        static final String TAG_SQLITE = "SQLiteException: ";
 
     }
     /* Inner class that defines the table contents */
-    public static abstract class FriendEntry {
-        public static final String TABLE_NAME = "friend";
-        public static final String COLUMN_ID = "_id";
-        public static final String COLUMN_EMAIL = "email";
-        public static final String COLUMN_NAME = "name";
+    static abstract class FriendEntry {
+        static final String TABLE_NAME = "friend";
+        static final String COLUMN_ID = "_id";
+        static final String COLUMN_EMAIL = "email";
+        static final String COLUMN_NAME = "name";
     }
 
-    public static abstract class TaskHeadEntry {
-        public static final String TABLE_NAME = "taskhead";
-        public static final String COLUMN_ID = "_id";
-        public static final String COLUMN_TITLE = "title";
-        public static final String COLUMN_MEMBERS = "members";
+    static abstract class TaskHeadEntry {
+        static final String TABLE_NAME = "taskhead";
+        static final String COLUMN_ID = "_id";
+        static final String COLUMN_TITLE = "title";
+        static final String COLUMN_MEMBERS = "members";
     }
 
-    public static abstract class TaskEntry {
-        public static final String TABLE_NAME = "task";
-        public static final String COLUMN_ID = "_id";
-        public static final String COLUMN_HEAD_ID = "taskheadid";
-        public static final String COLUMN_DESCRIPTION = "description";
-        public static final String COLUMN_COMPLETED = "completed";
-        public static final String COLUMN_ORDER = "sequence";
+    static abstract class TaskEntry {
+        static final String TABLE_NAME = "task";
+        static final String COLUMN_ID = "_id";
+        static final String COLUMN_HEAD_ID = "taskheadid";
+        static final String COLUMN_DESCRIPTION = "description";
+        static final String COLUMN_COMPLETED = "completed";
+        static final String COLUMN_ORDER = "sequence";
     }
 
 }
