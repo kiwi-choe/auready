a7c0167323454009aeffb07809d3f3f1d711cf9d
hacer03 <hacer03@naver.com>
2016-12-25 23:44:44 +0900

    Add ActionMode basic structure(just add override method) for TaskHeadView

--- app/src/main/java/com/kiwi/auready_ver2/taskheads/TaskHeadsAdapter.java
+++ app/src/main/java/com/kiwi/auready_ver2/taskheads/TaskHeadsAdapter.java
@@ -10,7 +10,9 @@ import android.widget.TextView;
 import com.kiwi.auready_ver2.R;
 import com.kiwi.auready_ver2.data.TaskHead;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -21,6 +23,7 @@ public class TaskHeadsAdapter extends BaseAdapter {
 
     private final TaskHeadsFragment.TaskHeadItemListener mItemListener;
     private List<TaskHead> mTaskHeads;
+    private HashMap<Integer, Boolean> mSelection = new HashMap<>();
 
     public TaskHeadsAdapter(List<TaskHead> taskHeads, TaskHeadsFragment.TaskHeadItemListener itemListener) {
         setList(taskHeads);
@@ -52,18 +55,26 @@ public class TaskHeadsAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
         View rowView = view;
         if (rowView == null) {
             LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
             rowView = inflater.inflate(R.layout.taskhead_item, viewGroup, false);
         }
 
-        final TaskHead taskHead = getItem(i);
+        final TaskHead taskHead = getItem(position);
         rowView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mItemListener.onTaskHeadItemClick(taskHead.getId());
+            }
+        });
+
+        rowView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                mItemListener.onTaskHeadItemLongClick(view, position);
+                return true;
             }
         });
 
@@ -76,6 +87,37 @@ public class TaskHeadsAdapter extends BaseAdapter {
                 mItemListener.onDeleteClick(taskHead);
             }
         });
+
+        if (mSelection.get(position) != null) {
+            rowView.setBackgroundColor(rowView.getResources().getColor(android.R.color.darker_gray));
+        } else {
+            rowView.setBackgroundColor(rowView.getResources().getColor(android.R.color.background_light));
+        }
+
         return rowView;
+    }
+
+    public void clearSelection() {
+        mSelection = new HashMap<>();
+        notifyDataSetChanged();
+    }
+
+    public void setNewSelection(int position, boolean checked) {
+        mSelection.put(position, checked);
+        notifyDataSetChanged();
+    }
+
+    public void removeSelection(int position) {
+        mSelection.remove(position);
+        notifyDataSetChanged();
+    }
+
+    public boolean isPositionChecked(int position) {
+        Boolean result = mSelection.get(position);
+        return result == null ? false : result;
+    }
+
+    public Set<Integer> getCurrentCheckedPosition() {
+        return mSelection.keySet();
     }
 }
